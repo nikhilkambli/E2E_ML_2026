d777109447ee58bafaa8d7342ef2e4683596f518
nikhil <niks0991@gmail.com>
2026-02-21 18:18:43 +0530

    data ingestion completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,41 @@
+import os
+import src
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+import sys
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path=os.path.join('artifact','train.csv')
+    test_data_path=os.path.join('artifact','test.csv')
+    raw_data_path=os.path.join('artifact','data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiated_data_ingestion(self):
+        logging.info('starting data ingestion....')
+        try:
+            df=pd.read_csv('E:/Machine Learning/E2E-AWS-Benstalk/notebook/data/stud.csv')
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info('Train test intiated')
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info('Injestion completed')
+            return (self.ingestion_config.train_data_path,
+                    self.ingestion_config.test_data_path,
+                    )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiated_data_ingestion()
